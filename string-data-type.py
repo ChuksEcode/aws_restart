5a98b7aa3304f6e2e7fdd878254a03311a32b9d4
aiincloud <119924609+aiincloud@users.noreply.github.com>
2022-12-06 09:56:16 +0200

    Add files via upload

---
+++ string-data-type.py
@@ -0,0 +1,13 @@
+myString = "This is a String"
+print(myString)
+print(type(myString))
+print(myString + " is of the data type " + str(type(myString)))
+firstString = "water"
+secondString = "fall"
+thirdStrin = firstString + secondString
+print(thirdStrin)
+name = input("what is your name? ")
+print(name)
+color = input("What is your favorite color? ")
+animal = input("What is your favorite animal? ")
+print("{}, you like a {} {}!".format(name,color,animal))
